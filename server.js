8996b9e790f9261562360ad8f2c645d311bc8f34
Artem Korolov <adkorolov@gmail.com>
2026-05-28 13:10:50 +0300

    feat: connect Supabase DB and implement saving strategies via POST

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const { Pool } = require('pg');
 const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
@@ -8,11 +10,30 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+const pool = new Pool({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: Number(process.env.DB_PORT),
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+pool.connect((err) => {
+	if (err) {
+		console.error('Помилка підключення до бази даних', err.stack);
+	} else {
+		console.log('Успішно підключено до бази даних')
+	}
+});
+
 app.get('/', (req, res) => {
 	res.send('<p>Вітаємо! Це бекенд сервер крипто-калькулятора. Він працює і чекає на запити з React.</p>')
 });
 
-app.post('/api/calculate', (req, res) => {
+app.post('/api/calculate', async (req, res) => {
 	const { coin, buyPrice, investSumUsdt, targetPercent } = req.body;
 
 	const averagingPrice = buyPrice * 0.90;
@@ -27,7 +48,7 @@ app.post('/api/calculate', (req, res) => {
 
 	const netProfit = sellSumAfter - investSumUsdt - transactionFee;
 
-	const strategyREsult = {
+	const strategyResult = {
 		id: crypto.randomUUID(),
 		coin: coin.toUpperCase(),
 		buyPrice,
@@ -40,7 +61,45 @@ app.post('/api/calculate', (req, res) => {
 		transactionFee,
 		netProfit
 	};
-	res.status(200).json(strategyREsult);
+	try {
+		const insertQuery = `
+			INSERT INTO strategies (
+				id,
+				coin,
+				buy_price,
+				sell_price,
+				averaging_price,
+				target_percent,
+				invest_sum_usdt,
+				sell_sum_after,
+				coin_amount,
+				transaction_fee,
+				net_profit
+			) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+		`;
+
+		const values = [
+			strategyResult.id,
+			strategyResult.coin,
+			strategyResult.buyPrice,
+			strategyResult.sellPrice,
+			strategyResult.averagingPrice,
+			strategyResult.targetPercent,
+			strategyResult.investSumUsdt,
+			strategyResult.sellSumAfter,
+			strategyResult.coinAmount,
+			strategyResult.transactionFee,
+			strategyResult.netProfit
+		];
+
+		await pool.query(insertQuery, values);
+		console.log(`Успішно збережено монету: ${strategyResult.coin}`);
+
+		res.status(200).json(strategyResult);
+	} catch (error) {
+		console.error('Помилка збереження стратегії в базу даних', error);
+		res.status(500).json({ error: 'Не вдалося зберегти дані в базу' });
+	}
 });
 
 app.listen(PORT, () => {
